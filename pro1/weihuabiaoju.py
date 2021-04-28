9047905e11a7361798d9a05c354a645292a2f9bc
wei.li <wei.li@molbase.com>
2021-04-28 16:36:20 +0800

    update

--- pro1/weihuabiaoju.py
+++ pro1/weihuabiaoju.py
@@ -25,6 +25,7 @@ import time
 import os
 import sys
 import requests
+from tools.tools1 import *
 
 # reload(sys)
 # sys.setdefaultencoding("utf-8")
@@ -84,28 +85,10 @@ def connect_mysql():
     server.select_db(MYSQL_DB)
 
 
-def today_time():
-    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
-def today_date():
-    return time.strftime("%Y-%m-%d", time.localtime(time.time()))
-def log_time():
-    return "[" +today_time()+ "] "
-def print_str(str):
-    print(log_time() + str)
-
 def j(extract_list):
     return "".join(extract_list.extract()).replace(":", "").strip()
 
-def create_counter():
-	def increase():
-		n = 5
-		while True:
-			n = n+5
-			yield n
-	it = increase()
-	def counter():
-		return next(it)
-	return counter
+
 counter_ = create_counter()
 
 def weihuabiaoju():
@@ -249,23 +232,7 @@ def main():
     logger.info("end")
     browser.quit()
 
-def post(param):
-    #jsoninfo = json.dumps(dict)  # 输出str类型
-    #dictinfo = json.loads(json_str)  # 输出dict类型
 
-    url = 'http://beta.napi.huayunquan.com/basic/whbj/simplePublishForPython'
-    # paramTest = {"goodsName": "测试1",
-    #              "goodsWight": "31",
-    #              "deliCity": "辽宁省,抚顺市",
-    #              "arriCity": "黑龙江省,大庆市",
-    #              "remark": "参考价格:160元,发布时间:1分钟前",
-    #              "reptileUrl": "https://www.weihuabiaoju.com/ptnpc/index.html#/goodsdetail?gsid=5402533403",
-    #              "reptileSource": "危化镖局"}
-    res = requests.post(url, json=param)  # 接口调用
-    resTest = json.loads(res.text)  # 将返回结果str转成dict
-    # {'code': 0, 'message': 'Success', 'data': None}
-    #print(resTest)
-    return resTest['code']
 
 
 def demo():
@@ -295,16 +262,11 @@ def demo():
 
     browser.quit()
 
-def get_time_interval():
-    a = datetime.now()  # 获得当前时间
-    time.sleep(22)  # 睡眠两秒
-    b = datetime.now()  # 获取当前时间
-    durn = (b - a).seconds  # 两个时间差，并以秒显示出来
-    print(durn)
 
 if __name__ == '__main__':
     #main()
     demo()
+
 
 
 
